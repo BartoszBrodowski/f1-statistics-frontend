9e4941bceb62d5b3c05fb28a87169b87c3dd121e
BartoszBrodowski <bartolini10058@gmail.com>
2023-08-14 00:24:39 +0200

    Initial Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between py-6">
+      <Navbar />
+    </main>
+  );
+}
