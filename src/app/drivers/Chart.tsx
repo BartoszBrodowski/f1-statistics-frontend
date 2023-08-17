795d6df525bb3cd23a6477c913927e5e8a6da776
BartoszBrodowski <bartolini10058@gmail.com>
2023-08-18 00:11:44 +0200

    feat: top nationalities chart

--- src/app/drivers/Chart.tsx
+++ src/app/drivers/Chart.tsx
@@ -11,7 +11,7 @@ import {
 } from "recharts";
 import { useQuery } from "@tanstack/react-query";
 import { Card } from "@/components/ui/card";
-import axios from "axios";
+import f1Api from "@/utils/axiosConfig";
 
 interface DriversData {
   nationality: string;
@@ -20,8 +20,9 @@ interface DriversData {
 
 const DriversChart: FC = ({}) => {
   const { data } = useQuery({
+    queryKey: ["drivers", "top", "nationalities"],
     queryFn: async () => {
-      const { data } = await axios.get(
+      const { data } = await f1Api.get(
         "http://localhost:5000/drivers/top/nationalities"
       );
       return data.slice(0, 7) as DriversData[];
@@ -29,47 +30,31 @@ const DriversChart: FC = ({}) => {
   });
 
   return (
-    <div className="flex gap-8 justify-center items-center w-full">
-      <Card className="w-2/5 py-6 pr-8">
-        <ResponsiveContainer width="100%" height={350}>
-          <BarChart data={data}>
-            <XAxis
-              dataKey="nationality"
-              stroke="#888888"
-              fontSize={12}
-              tickLine={false}
-              axisLine={false}
-            />
-            <YAxis
-              stroke="#888888"
-              fontSize={12}
-              tickLine={false}
-              axisLine={false}
-              tickFormatter={(value) => `${value}`}
-            />
-            <Tooltip
-              cursor={{ fill: "#333" }}
-              contentStyle={{ color: "black" }}
-            />
-            <Bar dataKey="count" fill="red" radius={[4, 4, 0, 0]} />
-          </BarChart>
-        </ResponsiveContainer>
-      </Card>
-      <div className="flex flex-col gap-6 w-[45%]">
-        <h1 className="text-4xl font-bold">Top Nationalities</h1>
-        <div className="text-gray9 flex flex-col gap-2">
-          <p>
-            Every driver represents a place of their origin. Some drivers have
-            more than one nationality, therefore they have to declare the
-            nationality they represent.
-          </p>
-          <p>
-            Alexander Albon, despite being born and raised in Great Britain,
-            chose to represent Thailand embracing his Thai identity.
-          </p>
-        </div>
-      </div>
-    </div>
+    <Card className="w-2/5 py-6 pr-8">
+      <ResponsiveContainer width="100%" height={350}>
+        <BarChart data={data}>
+          <XAxis
+            dataKey="nationality"
+            stroke="#888888"
+            fontSize={12}
+            tickLine={false}
+            axisLine={false}
+          />
+          <YAxis
+            stroke="#888888"
+            fontSize={12}
+            tickLine={false}
+            axisLine={false}
+            tickFormatter={(value) => `${value}`}
+          />
+          <Tooltip
+            cursor={{ fill: "transparent" }}
+            contentStyle={{ color: "black" }}
+          />
+          <Bar dataKey="count" fill="red" radius={[4, 4, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
   );
 };
 
--- src/app/drivers/page.tsx
+++ src/app/drivers/page.tsx
@@ -1,11 +1,38 @@
 import { FC } from "react";
 import Chart from "./Chart";
+import { Text, Heading, Box, Flex, CalloutIcon } from "@radix-ui/themes";
+import FunFactBox from "@/components/FunfactBox";
+import ReadMore from "@/components/ReadMore";
 
 const Drivers: FC = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <Box className="flex gap-8 justify-center items-center min-h-screen">
       <Chart />
-    </div>
+      <Flex direction="column" gap="4" pl="6" className="w-[45%]">
+        <Heading size="8" className="dark:text-white">
+          Top Nationalities
+        </Heading>
+        <Flex direction="column" className="text-gray9">
+          <Heading size="5">
+            The chart on the left shows the top 7 nationalities with the most
+            drivers in Formula 1 history.
+          </Heading>
+        </Flex>
+        <ReadMore>
+          <Flex direction="column" gap="2">
+            <Text size="4" className="dark:text-white">
+              Every driver represents a place of their origin. Some drivers have
+              more than one nationality, therefore they have to declare the
+              nationality they represent.
+            </Text>
+            <FunFactBox>
+              Alexander Albon, despite being born and raised in Great Britain,
+              chose to represent Thailand embracing his Thai identity.
+            </FunFactBox>
+          </Flex>
+        </ReadMore>
+      </Flex>
+    </Box>
   );
 };
 
